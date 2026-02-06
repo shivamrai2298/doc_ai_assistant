b9adc7f781d86057e45d04716673ab9818cd08f5
Shivam Rai <shivamrai2298@gmail.com>
2026-02-06 16:54:54 +0530

    Update ocr.py

--- doc_ai_assistant/ingestion/ocr.py
+++ doc_ai_assistant/ingestion/ocr.py
@@ -1,1 +1,12 @@
+import pytesseract
+from pdf2image import convert_from_bytes
+
+def ocr_extract(file):
+    images = convert_from_bytes(file.file.read())
+    text = ""
+
+    for img in images:
+        text += pytesseract.image_to_string(img)
+
+    return text
 
