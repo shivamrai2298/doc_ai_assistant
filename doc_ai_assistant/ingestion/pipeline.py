fc6aca433023c0cdebf25732b9598355383108a4
Shivam Rai <shivamrai2298@gmail.com>
2026-02-06 16:49:30 +0530

    Update pipeline.py

--- doc_ai_assistant/ingestion/pipeline.py
+++ doc_ai_assistant/ingestion/pipeline.py
@@ -1,1 +1,12 @@
+from app.ingestion.pdf_loader import extract_text
+from app.ingestion.chunking import chunk_text
+
+def ingest_document(file):
+    # Step 1: PDF se text nikalo
+    text = extract_text(file)
+
+    # Step 2: Text ko chhote parts me todo
+    chunks = chunk_text(text)
+
+    return f"Document processed. Total chunks: {len(chunks)}"
 
