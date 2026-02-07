75754a31e7f7d05fe288e34b81302d8f95ea7076
Shivam Rai <shivamrai2298@gmail.com>
2026-02-07 15:06:06 +0530

    Update embeddings.py

--- doc_ai_assistant/rag/embeddings.py
+++ doc_ai_assistant/rag/embeddings.py
@@ -1,1 +1,13 @@
+from sentence_transformers import SentenceTransformer
+
+# Model memory me ek hi baar load hoga
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def generate_embeddings(chunks):
+    """
+    chunks: list of strings
+    returns: list of vectors (numbers)
+    """
+    embeddings = model.encode(chunks)
+    return embeddings
 
