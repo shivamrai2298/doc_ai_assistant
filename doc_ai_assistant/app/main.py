1df0cdcecbce6b6e1f87b0d5019da34cc9ee3883
Shivam Rai <shivamrai2298@gmail.com>
2026-02-07 15:44:58 +0530

    Update main.py

--- doc_ai_assistant/app/main.py
+++ doc_ai_assistant/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app.api.routes.ingest import router as ingest_router
+from app.api.routes.query import router as query_router
 
 app = FastAPI()
 
 app.include_router(ingest_router, prefix="/api")
-
+app.include_router(query_router, prefix="/api")
