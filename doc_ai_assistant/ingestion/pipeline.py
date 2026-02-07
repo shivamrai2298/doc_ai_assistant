516e2dde7b02a9066493ed6e525ddbb8ac831818
Shivam Rai <shivamrai2298@gmail.com>
2026-02-07 15:07:32 +0530

    Update pipeline.py

--- doc_ai_assistant/ingestion/pipeline.py
+++ doc_ai_assistant/ingestion/pipeline.py
@@ -1,9 +1,19 @@
 from app.ingestion.pdf_loader import extract_text
 from app.ingestion.chunking import chunk_text
+from rag.embeddings import generate_embeddings
 
 def ingest_document(file):
+    # Step 1: Text extract
     text = extract_text(file)
+
+    # Step 2: Chunking
     chunks = chunk_text(text)
 
-    return f"Document processed. Total chunks: {len(chunks)}"
+    # Step 3: Embeddings
+    embeddings = generate_embeddings(chunks)
 
+    return f"""
+    Document processed.
+    Chunks: {len(chunks)}
+    Embeddings created: {len(embeddings)}
+    """
