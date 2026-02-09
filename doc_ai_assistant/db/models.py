d28d6dca0dbaaa37fe45177919f4bd138bdabed8
Shivam Rai <shivamrai2298@gmail.com>
2026-02-09 16:19:44 +0530

    Update models.py

--- doc_ai_assistant/db/models.py
+++ doc_ai_assistant/db/models.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id = Column(Integer, primary_key=True)
+    filename = Column(String, nullable=False)
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
 
