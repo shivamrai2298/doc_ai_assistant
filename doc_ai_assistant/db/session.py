c3253c1e16b0046ef506a9c22bcd28ec8a2cb92b
Shivam Rai <shivamrai2298@gmail.com>
2026-02-09 16:19:56 +0530

    Update session.py

--- doc_ai_assistant/db/session.py
+++ doc_ai_assistant/db/session.py
@@ -1,1 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./doc_ai.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
