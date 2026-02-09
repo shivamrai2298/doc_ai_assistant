0fda930521906d5a09b4ddd5ea5ca54ba9163758
Shivam Rai <shivamrai2298@gmail.com>
2026-02-09 16:20:16 +0530

    Update ner.py

--- doc_ai_assistant/nlp/ner.py
+++ doc_ai_assistant/nlp/ner.py
@@ -1,1 +1,14 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def extract_entities(text: str):
+    doc = nlp(text)
+    return [
+        {
+            "text": ent.text,
+            "label": ent.label_
+        }
+        for ent in doc.ents
+    ]
 
