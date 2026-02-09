5a146e7bb5202652b757ce4378b6fdeb8fd62b3a
Shivam Rai <shivamrai2298@gmail.com>
2026-02-09 16:15:49 +0530

    Update streamlit_app.py

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import requests
+
+st.title("📄 Doc AI Assistant")
+
+uploaded_file = st.file_uploader("Upload PDF")
+
+if uploaded_file:
+    res = requests.post(
+        "http://localhost:8000/ingest",
+        files={"file": uploaded_file}
+    )
+    st.success(res.json())
+
+query = st.text_input("Ask a question")
+
+if query:
+    res = requests.post(
+        "http://localhost:8000/query",
+        json={"question": query}
+    )
+    st.write(res.json()["answer"])
 
